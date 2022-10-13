950dd1f29f0eed9b2b6b43d47634a60210829cef
mako542b <marcin.piotr.konopka@gmail.com>
2022-10-13 16:15:00 +0200

    final commit

--- public/script.js
+++ public/script.js
@@ -4,5 +4,16 @@ let navList = document.querySelector('#navList')
 
 
 hamburgerButton.addEventListener('click', () => {
+    hamburgerButton.toggleAttribute('aria-expanded')
     nav.classList.toggle('hidden')
+})
+
+nav.addEventListener('click', (e) => {
+    if(!hamburgerButton.hasAttribute('aria-expanded')){
+        return
+    }
+    if(e.target !== navList){
+        hamburgerButton.toggleAttribute('aria-expanded')
+        nav.classList.toggle('hidden')
+    }
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,20 @@ module.exports = {
     extend: {
       fontFamily: {
         'public': ['Public Sans', 'sans-serif']
+      },
+      colors: {
+        primaryDarkBlue: 'hsl(233, 26%, 24%)',
+        primaryLimeGreen: 'hsl(136, 65%, 51%)',
+        primaryBrightCyan: 'hsl(192, 70%, 51%)',
+        neutralGrayishBlue: 'hsl(233, 8%, 62%)',
+        neutralLightGrayishBlue: 'hsl(220, 16%, 96%)',
+        neutralVeryLightGray: 'hsl(0, 0%, 98%)',
+        neutralWhite: 'hsl(0, 0%, 100%)'
+      },
+      fontSize: {
+        h1: ['3.35rem', '4rem'],
+        h2: ['2.5rem', '3.125rem'],
+        ss: ['.8125rem', '1.125rem']
       }
     },
   },
